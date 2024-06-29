146705c28f35f59a0bda50edd0602579ad21f206
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-06-29 16:48:26 +0300

    task 1 final commit

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,10 @@
+import { CircleLoader } from 'react-spinners';
+import style from './Loader.module.css';
+
 export const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className={style.backdrop}>
+      <CircleLoader size="150" color="red" />
+    </div>
+  );
 };
